9fa31217c026359461c8dde9b60d77113f24ea93
Vahe Saroyan <vahe.saroyan.web@gmail.com>
2021-01-06 14:21:20 +0400

    Added LogBox

--- template/index.js
+++ template/index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native'
+import { AppRegistry,LogBox } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
 
+LogBox.ignoreAllLogs();
 AppRegistry.registerComponent(appName, () => App)
