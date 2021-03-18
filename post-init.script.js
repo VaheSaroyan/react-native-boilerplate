e25d95e81e96c4e0d0d68b584b6d2e95822eb02f
Vahe Saroyan <vahe.saroyan.web@gmail.com>
2021-03-18 16:33:24 +0400

    Update post-init.script.js

--- post-init.script.js
+++ post-init.script.js
@@ -1,19 +1,5 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-
-fs.copyFile('src/config/index.example.js', 'src/config/index.js', (err) => {
-    if (err) throw err;
-    console.log('src/config/index.example.js was copied to src/config/index.js');
-});
-
-fs.unlink('src/config/index.example.js', (err) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    console.log('src/config/index.example.js was removed');
-})
 
 console.log("React-Native Boilerplate initialized with success !");
 
