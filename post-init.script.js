4ab50c95c3d5d147e990ece0cebefc87506af241
Vahe Saroyan <vahe.saroyan.web@gmail.com>
2022-02-20 17:11:12 +0400

    New structure for boilerplate

--- post-init.script.js
+++ post-init.script.js
@@ -1,22 +1,29 @@
 #!/usr/bin/env node
+const { green, blue } = require('kleur')
+
+console.log(blue("VS React-Native Boilerplate initialized with success !  🚀\n"));
 
 
-console.log("React-Native Boilerplate initialized with success !");
+console.log(green(`
+__/\\\\\\________/\\\\\\_____/\\\\\\\\\\\\\\\\\\\\\\___________________        
+ _\\/\\\\\\_______\\/\\\\\\___/\\\\\\/////////\\\\\\_________________       
+  _\\//\\\\\\______/\\\\\\___\\//\\\\\\______\\///__________________      
+   __\\//\\\\\\____/\\\\\\_____\\////\\\\\\_________________________     
+    ___\\//\\\\\\__/\\\\\\_________\\////\\\\\\______________________    
+     ____\\//\\\\\\/\\\\\\_____________\\////\\\\\\___________________   
+      _____\\//\\\\\\\\\\_______/\\\\\\______\\//\\\\\\__________________  
+       ______\\//\\\\\\_______\\///\\\\\\\\\\\\\\\\\\\\\\/______________/\\\\\\_ 
+        _______\\///__________\\///////////_______________\\///__
+`))
 
+console.log('\n')
 
 console.log(
-    `
-   VSVS                 VSVS        SVSVSVSVSSVSVSVSV
-   VSVS                 VSVS      VSVSVSVVSVSVSVSVSVSV
-    VSVS               VSVS         VVSVSV       VSVS 
-     VSVS             VSVS             VVSVS  
-      VSVS           VSVS                VSVSVS 
-       VSVS         VSVS                   VSVSVS 
-        VSVS       VSVS                      VVSVSV 
-         VSVS     VSVS             VSVS       VVSVSV 
-          VSVS   VSVS             VSVSVSVVSVSVSVSVSVSV         VSVS
-           VSVS VSVS               VSVSVSVSSVSVSVSVS           VSVS
-  `,
+    '- 📚 If you need to read more about this boilerplate : https://vahesaroyan.github.io/react-native-boilerplate',
 )
-
-
+console.log(
+    '- 🤕 If you have some troubles : https://github.com/VaheSaroyan/react-native-boilerplate/issues',
+)
+console.log(
+    '- ⭐ If you love this boilerplate, give us a star, you will be a ray of sunshine in our lives :) https://github.com/VaheSaroyan/react-native-boilerplate',
+)
