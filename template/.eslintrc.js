e20b9913a9f3ec403955215e80eb8bd4aba55e0c
Vahe Saroyan <vahe.saroyan@codsbe.com>
2024-03-16 16:19:10 +0400

    added new files to template

---
+++ template/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: ['universe', 'universe/shared/typescript-analysis'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  ],
+  plugins: ['import', 'prettier'],
+  rules: {
+    'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
+    'import/order': [
+      'error',
+      {
+        groups: [['external', 'builtin'], 'internal', ['sibling', 'parent'], 'index'],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before'
+          },
+          {
+            pattern: '@/**',
+            group: 'internal'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ],
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true
+      }
+    ]
+  }
+};
---
+++ template/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidth: 2,
+  trailingComma: 'none',
+  bracketSameLine: true,
+  singleQuote: true,
+  printWidth: 100,
+  arrowParens: 'avoid'
+};
